f721acfed8079107afe32e3c6314773eca806ac5
Mohan Gond <mohan.gond@cognizant.com>
2024-04-03 15:39:26 +0530

    first commit

--- src/describeContactFlow.js
+++ src/describeContactFlow.js
@@ -17,4 +17,4 @@ async function describeContactFlow(instanceId, flowId, region) {
     // Continue with the rest of your logic...
 }
 
-describeContactFlow(process.env.INSTANCEARN, process.env.FLOWID, process.env.Region);
+describeContactFlow('arn:aws:connect:us-east-1:750344256621:instance/4bbee21d-72b8-442b-af39-dce4128ca77e', 'a222d77e-f37a-42f6-b00e-9a3a1671e9bc', 'us-east-1');
