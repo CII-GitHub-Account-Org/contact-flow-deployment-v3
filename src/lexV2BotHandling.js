32f0131b81bbd3ce957484235d21290f330729a5
Mohan Gond <mohan.gond@cognizant.com>
2024-04-23 19:01:10 +0530

    CI/CD Deployment

--- src/lexV2BotHandling.js
+++ src/lexV2BotHandling.js
@@ -1,6 +1,7 @@
 
 import { LexModelsV2Client, DescribeBotCommand, ListBotsCommand } from "@aws-sdk/client-lex-models-v2";
 let regionToUse;
+import writeDataToFile from './writeDataToFile.js';
 
 export default async function lexV2BotHandling(primaryLexBot, aliasArn, targetLexBot, region) {
     regionToUse = region;
@@ -73,8 +74,16 @@ export default async function lexV2BotHandling(primaryLexBot, aliasArn, targetLe
       }
     }
 
+    const inputListLexV2Bots = { // ListBotsRequest
+      sortBy: { // BotSortBy
+        attribute: "BotName", // required
+        order: "Ascending", // required
+      },
+      maxResults: 10
+    };
+
     if (!foundAliasArnInTarget) {
-     const listLexV2BotsResponse = await listLexV2Bots(regionToUse);
+     const listLexV2BotsResponse = await listLexV2BotsFunc(regionToUse,inputListLexV2Bots);
      console.log('listLexV2Bots : ', listLexV2BotsResponse);
     }
 
@@ -108,16 +117,50 @@ async function getlexV2BotName (aliasArn, region) {
     return botName;
 }
 
-async function listLexV2Bots (region) {
+
+
+async function listLexV2Bots (region,params) {
+  let responseListLexV2Bots = [];
   const client = new LexModelsV2Client({ region: region });
-  const inputListLexV2Bots = { // ListBotsRequest
-    sortBy: { // BotSortBy
-      attribute: "BotName", // required
-      order: "Ascending", // required
-    },
-    maxResults: 10
-  };
-const commandListLexV2Bots = new ListBotsCommand(inputListLexV2Bots);
-const responseListLexV2Bots = await client.send(commandListLexV2Bots);
-return responseListLexV2Bots;
+  let commandListLexV2Bots = new ListBotsCommand(params);
+  let response = await client.send(commandListLexV2Bots);
+  responseListLexV2Bots.push(response);
+  while (response.nextToken) {
+    params.nextToken = response.nextToken;
+    commandListLexV2Bots = new ListBotsCommand(params);
+    response = await client.send(commandListLexV2Bots);
+    responseListLexV2Bots.push(response);
+  }
+  return responseListLexV2Bots;
 }
+
+  // Helper function to handle throttling
+  export async function listLexV2BotsFunc(region, params) {
+    try {
+      let doRetry = false;
+      do {
+        doRetry = false;
+        try {
+          const listResources = await listLexV2Bots(region, params);
+          if (listResources) {
+            return listResources;
+          } else {
+            return null;
+          }
+        } catch (error) {
+          console.log('error:', error);
+          if (error.code === 'TooManyRequestsException' && (retryAttempts || 3) > 0) {
+            await sleep(parseInt(2500, 10) || 1000);
+            --retryAttempts;
+            doRetry = true;
+            console.log('doRetry:', doRetry);
+          } else {
+            return error;
+          }
+        }
+      } while (doRetry);
+    } catch (error) {
+      console.log('error:', error);
+      return error;
+    }
+  };
