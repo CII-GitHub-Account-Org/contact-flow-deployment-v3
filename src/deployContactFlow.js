ccea298de3ea962572940dc54d6f972174c0d69a
Mohan Gond <mohan.gond@cognizant.com>
2024-04-30 11:51:18 +0530

    CI/CD Deployment

--- src/deployContactFlow.js
+++ src/deployContactFlow.js
@@ -190,38 +190,27 @@ for (let i = 0; i < contentActions.length; i++) {
         else if (obj.Parameters.EventHooks.CustomerQueue) {
           console.log('Inside Customer Queue Handling');
           console.log('obj : ', obj);
-          // obj :  {
-          //   Parameters: {
-          //     EventHooks: {
-          //       CustomerQueue: 'arn:aws:connect:us-east-1:***:instance/4bbee21d-72b8-442b-af39-dce4128ca77e/contact-flow/4aaf1c69-6bf9-4312-9db8-c1b585cc2664'
-          //     }
-          //   },
-          //   Identifier: '4d3295c8-7f19-4646-9bb0-1fbb25802a5c',
-          //   Type: 'UpdateContactEventHooks',
-          //   Transitions: {
-          //     NextAction: '2687fd55-d81d-4372-8b6b-0a4bafa0bd08',
-          //     Errors: [ [Object] ]
-          //   }
-          // }
-        
           const subCustomerQueueFlowArn = obj && obj.Parameters && obj.Parameters.EventHooks && obj.Parameters.EventHooks.CustomerQueue ? obj.Parameters.EventHooks.CustomerQueue : undefined;
           console.log('subCustomerQueueFlowArn : ', subCustomerQueueFlowArn);
           const subContactFlowHandlingRes = await subContactFlowHandling(primaryContactFlows, subCustomerQueueFlowArn, targetContactFlows, instanceArn, sourceRegion, targetRegion);
           console.log('subContactFlowHandlingRes : ', subContactFlowHandlingRes);
           await writeDataToFile('subContactFlowHandlingRes.json', subContactFlowHandlingRes);
-          // if (targetQueueResources && targetQueueResources.ResourceStatus === 'exists') {
-          //   targetJson = targetJson.replace(new RegExp(queueArn, 'g'), targetQueueResources.ResourceArn);
-          // } else if (targetQueueResources && targetQueueResources.ResourceStatus === 'notExists') {
-          //   missedResourcesInTarget.push({
-          //       "flowName": flowName,
-          //       "ResourceType": targetQueueResources.ResourceType,
-          //       "ResourceName": targetQueueResources.ResourceName,
-          //       "ResourceArn": targetQueueResources.ResourceArn
-          //   });
-          // }
-            // let arn = getFlowId(PRIMARYCFS, obj.Parameters.EventHooks.CustomerQueue, TARGETCFS);
-            // if (arn) {TARGETJSON = TARGETJSON.replace(new RegExp(obj.Parameters.EventHooks.CustomerQueue, 'g'), arn)};
-          } 
+          const getMissedResourcesResCustomerQueue = await getMissedResources(subContactFlowHandlingRes.targetJsonSubContactFlow, subContactFlowHandlingRes.contentActionsSubContactFlow, subContactFlowHandlingRes.primarySubContactFlowName, primaryQueues, targetQueues, 
+            primaryHOP, targetHOP, primaryLexBot, targetLexBot, primaryLambda, targetLambda, sourceRegion, targetRegion);
+            console.log('getMissedResourcesResCustomerQueue : ', getMissedResourcesResCustomerQueue);
+            missedResourcesInTarget = missedResourcesInTarget.concat(getMissedResourcesResCustomerQueue.missedResourcesInTarget);
+           priority = priority + 1;
+            arrayToCreateOrUpdateFlow.push({
+            "isExist": subContactFlowHandlingRes.isExists,
+            "flowName": subContactFlowHandlingRes.primarySubContactFlowName,
+            "targetInstanceArn": targetInstanceArn,
+            "contactFlowType": subContactFlowHandlingRes.primarySubContactFlowType,
+            "targetJson": getMissedResourcesResCustomerQueue.targetJson,
+            "targetFlowId": subContactFlowHandlingRes.targetSubContactFlowId,
+            "targetRegion": targetRegion,
+            "priority": priority
+          }); 
+        } 
         else if (obj.Parameters.EventHooks.CustomerRemaining) {
           console.log('Inside Customer Remaining Handling');
           console.log('obj : ', obj);
@@ -239,7 +228,8 @@ for (let i = 0; i < contentActions.length; i++) {
         }
 }
 
-
+await writeDataToFile('missedResourcesInTargetUpdated.json', missedResourcesInTarget);
+await writeDataToFile('arrayToCreateOrUpdateFlowUpdated.json', arrayToCreateOrUpdateFlow);
 await writeDataToFile('targetJsonUpdated.json', JSON.parse(targetJson));
 contentActions = JSON.parse(targetJson).Actions;
 // console.log("contentActions After Replacing", contentActions);
